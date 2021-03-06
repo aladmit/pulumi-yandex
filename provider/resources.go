c0c24ce6d3673786b391993cbc962a8b78d90208
Andrey Aleksandrov <aleksandrov@hey.com>
2021-03-07 00:26:26 +0300

    fix of import path in provider

--- provider/resources.go
+++ provider/resources.go
@@ -160,7 +160,7 @@ func Provider() tfbridge.ProviderInfo {
 		},
 		Golang: &tfbridge.GolangInfo{
 			ImportBasePath: filepath.Join(
-				fmt.Sprintf("github.com/pulumi/pulumi-%[1]s/sdk/", mainPkg),
+				fmt.Sprintf("github.com/aladmit/pulumi-%[1]s/sdk/", mainPkg),
 				tfbridge.GetModuleMajorVersion(version.Version),
 				"go",
 				mainPkg,
